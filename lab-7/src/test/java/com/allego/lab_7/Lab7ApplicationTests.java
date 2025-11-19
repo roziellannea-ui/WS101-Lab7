3eddd2c348cf908008070a3846491b7732f45131
roziellannea-ui <roziellannea@gmail.com>
2025-11-19 12:25:36 +0800

    Commit

---
+++ lab-7/src/test/java/com/allego/lab_7/Lab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allego.lab_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
